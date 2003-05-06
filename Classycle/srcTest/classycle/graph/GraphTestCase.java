325ec779d4e3cdda9ab6f3b1b3b5e42faf326467
fjelmer <fjelmer@39be711e-0c1e-0410-9903-fe3f45c2df2f>
2003-05-06 08:54:08 +0000

    add  - method check

--- Classycle/srcTest/classycle/graph/GraphTestCase.java
+++ Classycle/srcTest/classycle/graph/GraphTestCase.java
@@ -1,69 +1,78 @@
 package classycle.graph;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 import junit.framework.TestCase;
 
-public class GraphTestCase extends TestCase
-{
-  private static class MockAttributes implements Attributes
-  {
+public class GraphTestCase extends TestCase {
+  static class MockAttributes implements Attributes {
     private final String _name;
-    public MockAttributes(String name)
-    {
+    public MockAttributes(String name) {
       _name = name;
     }
 
-    public String toString()
-    {
+    public String toString() {
       return _name;
     }
   }
 
-  protected Vertex[] _cyclicGraph, _acyclicGraph;
-  protected Vertex _externalVertex = new Vertex(null);
+  protected AtomicVertex _externalVertex = new AtomicVertex(null);
 
-  public GraphTestCase(String s)
-  {
+  public GraphTestCase(String s) {
     super(s);
   }
 
-  protected void setUp() throws Exception
-  {
-    _cyclicGraph = createGraph(new int[][] {{1},                 // 0
-                                            {2},                 // 1
-                                            {2, 3, -1, 4},       // 2
-                                            {1, -1, 0, 6},       // 3
-                                            {5},                 // 4
-                                            {4},                 // 5
-                                            {3, 7},              // 6
-                                            {-1}                 // 7
-                                           });
-    _acyclicGraph = createGraph(new int[][] {{1, -1, -1, 2},     // 0
-                                             {},                 // 1
-                                             {1},                // 2
-                                             {4, 1, -1, 5},      // 3
-                                             {2},                // 4
-                                             {1, -1}             // 5
-                                            });
-  }
-
-  protected Vertex[] createGraph(int[][] nodeLinks)
-  {
-    Vertex[] result = new Vertex[nodeLinks.length];
-    for (int i = 0; i < result.length; i++)
-    {
-      result[i] = new Vertex(new MockAttributes(Integer.toString(i)));
+  protected void check(String[] expectedStrongComponents, int[][] nodeLinks) {
+    HashSet expectedFingerPrints = new HashSet();
+    for (int i = 0; i < expectedStrongComponents.length; i++) {
+      expectedFingerPrints.add(expectedStrongComponents[i]);
     }
-    for (int i = 0; i < result.length; i++)
-    {
+    AtomicVertex[] graph = createGraph(nodeLinks);
+    StrongComponentProcessor processor = new StrongComponentProcessor();
+    process(processor, graph);
+    StrongComponent[] components = processor.getStrongComponents();
+    for (int i = 0; i < components.length; i++) {
+      String fingerPrint = createFingerPrint(components[i]);
+      assertTrue("'" + fingerPrint + "' not expected", 
+                 expectedFingerPrints.contains(fingerPrint));
+    }
+    assertEquals("number of strong components", 
+                 expectedStrongComponents.length, components.length);
+  }
+  
+  protected void process(StrongComponentProcessor processor, 
+                         AtomicVertex[] graph) {
+    processor.deepSearchFirst(graph);
+  }
+  
+  private String createFingerPrint(StrongComponent component) {
+    int[] vertices = new int[component.getNumberOfVertices()];
+    for (int i = 0; i < vertices.length; i++) {
+      vertices[i] = Integer.parseInt(((MockAttributes) component.getVertex(i)
+                                     .getAttributes()).toString());
+    }
+    Arrays.sort(vertices);
+    StringBuffer result = new StringBuffer();
+    result.append(component.getLongestWalk()).append(':');
+    for (int i = 0; i < vertices.length; i++) {
+      result.append(' ').append(Integer.toString(vertices[i]));
+    }
+    return new String(result);
+  }
+  protected AtomicVertex[] createGraph(int[][] nodeLinks) {
+    AtomicVertex[] result = new AtomicVertex[nodeLinks.length];
+    for (int i = 0; i < result.length; i++) {
+      result[i] = new AtomicVertex(new MockAttributes(Integer.toString(i)));
+    }
+    for (int i = 0; i < result.length; i++) {
       int[] links = nodeLinks[i];
-      for (int j = 0; j < links.length; j++)
-      {
+      for (int j = 0; j < links.length; j++) {
         int link = links[j];
         result[i].addOutgoingArcTo(link < 0 ? _externalVertex : result[link]);
       }
     }
     return result;
   }
-
 
 } //class
