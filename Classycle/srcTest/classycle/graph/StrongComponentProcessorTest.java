b131ea36cb90ce61a9bb467e5929b2f7642a3d3a
fjelmer <fjelmer@39be711e-0c1e-0410-9903-fe3f45c2df2f>
2003-05-13 17:04:03 +0000

    add  - testAttributes

--- Classycle/srcTest/classycle/graph/StrongComponentProcessorTest.java
+++ Classycle/srcTest/classycle/graph/StrongComponentProcessorTest.java
@@ -82,16 +82,57 @@ public class StrongComponentProcessorTest extends GraphTestCase {
                        {3, 7},              // 6
                        {-1}                 // 7
                       });
-    check(new String[] {"0: 0", "0: 1", "0: 2 6", "0: 4", "0: 3 7 8", "0: 5"},
-          new int[][] {{2},                 // 0
-                       {-1},                // 1
-                       {1, 6, -1},          // 2
-                       {7, -1, 4},          // 3
-                       {6},                 // 4
-                       {1},                 // 5
-                       {2, -1},             // 6
-                       {8, 5, -1},          // 7
-                       {3, -1}              // 8
-                      });
+  }
+  
+  public void testAttributesOfStrongComponent() {
+    StrongComponent[] components
+        = check(new String[] {"0: 0 1 2 3 6", "0: 4 5", "0: 7"},
+                new int[][] {{1},                 // 0
+                             {2},                 // 1
+                             {2, 3, -1, 4},       // 2
+                             {1, -1, 0, 6},       // 3
+                             {5},                 // 4
+                             {4},                 // 5
+                             {3, 7},              // 6
+                             {-1}                 // 7
+                            });
+    checkAttributes(new int[] {4, 2, 2, 1, 1, 1, 4, 1},
+                    new int[] {4, 3, 2, 2, 1, 1, 3, 0}, components);
+
+    components
+        = check(new String[] {"0: 0", "0: 1", "0: 2 6", "0: 4", "0: 3 7 8 9", 
+                              "0: 5"},
+                new int[][] {{2},                 // 0
+                             {-1},                // 1
+                             {1, 6, -1},          // 2
+                             {7, -1, 4, 9},       // 3
+                             {6},                 // 4
+                             {1},                 // 5
+                             {2, -1},             // 6
+                             {8, 5, -1},          // 7
+                             {3, -1},             // 8
+                             {3, -1}              // 9
+                            });
+    checkAttributes(new int[] {1, 1, 1, 1, 1, 1, 1, 2, 2, 3},
+                    new int[] {0, 0, 1, 2, 0, 0, 1, 3, 2, 3}, components);
+  }
+  
+  private void checkAttributes(int[] expectedMaximumFragmentSizes,
+                               int[] expectedEccentricities, 
+                               StrongComponent[] components) {
+    for (int i = 0; i < components.length; i++) {
+      StrongComponent component = components[i];
+      GraphAttributes attributes = (GraphAttributes) component.getAttributes();
+      for (int j = 0, n = component.getNumberOfVertices(); j < n; j++) {
+        int index = Integer.parseInt(component.getVertex(j)
+                                              .getAttributes().toString());
+        assertEquals("eccentricity of vertex " + index, 
+                     expectedEccentricities[index],
+                     n == 1 ? 0 : attributes.getEccentricities()[j]);
+        assertEquals("max fragment size of vertex " + index, 
+                     expectedMaximumFragmentSizes[index],
+                     attributes.getMaximumFragmentSizes()[j]);
+      }
+    }
   }
 }
