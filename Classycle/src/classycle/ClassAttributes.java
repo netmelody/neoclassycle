eadfbfaf9f1ecee3010a25744c6f9bf4a125de30
fjelmer <fjelmer@39be711e-0c1e-0410-9903-fe3f45c2df2f>
2004-06-11 18:14:11 +0000

    change copyright years

--- Classycle/src/classycle/ClassAttributes.java
+++ Classycle/src/classycle/ClassAttributes.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, Franz-Josef Elmer, All rights reserved.
+ * Copyright (c) 2003-2004, Franz-Josef Elmer, All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
