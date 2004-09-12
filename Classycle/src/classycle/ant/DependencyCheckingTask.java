735d9645560616a292bb83b3459708022d16b893
fjelmer <fjelmer@39be711e-0c1e-0410-9903-fe3f45c2df2f>
2004-09-12 15:50:51 +0000

    add

---
+++ Classycle/src/classycle/ant/DependencyCheckingTask.java
@@ -0,0 +1,86 @@
+/*
+ * Created on 12.09.2004
+ */
+package classycle.ant;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.apache.tools.ant.BuildException;
+
+import classycle.dependency.DefaultResultRenderer;
+import classycle.dependency.DependencyChecker;
+import classycle.dependency.ResultRenderer;
+import classycle.util.Text;
+
+/**
+ * @author  Franz-Josef Elmer
+ */
+public class DependencyCheckingTask extends ClassycleTask
+{
+  private String _definitionFile;
+  private String _dependencyDefinition;
+  private String _resultRenderer;
+  
+  public void setDefinitionFile(String definitionFile)
+  {
+    _definitionFile = definitionFile;
+  }
+
+  public void setResultRenderer(String resultRenderer)
+  {
+    _resultRenderer = resultRenderer;
+  }
+  
+  public void addText(String text) 
+  {
+    _dependencyDefinition = text.trim();
+  }
+  
+  public void execute() throws BuildException
+  {
+    super.execute();
+    
+    boolean ok = false;
+    try
+    {
+      DependencyChecker dependencyChecker 
+          = new DependencyChecker(getClassFileNames(), getPattern(), 
+                                  getDependencyDefinitions(), getRenderer());
+      PrintWriter printWriter = new PrintWriter(System.out);
+      ok = dependencyChecker.check(printWriter);
+      printWriter.flush();
+    } catch (Exception e)
+    {
+      throw new BuildException(e);
+    }
+    if (ok == false) {
+      throw new BuildException("Invalid dependencies found.");
+    }
+  }
+
+  private ResultRenderer getRenderer() throws InstantiationException, IllegalAccessException, ClassNotFoundException
+  {
+    ResultRenderer renderer = new DefaultResultRenderer();
+    if (_resultRenderer != null) 
+    {
+      renderer = (ResultRenderer) Class.forName(_resultRenderer).newInstance();
+    }
+    return renderer;
+  }
+
+  private String getDependencyDefinitions() throws IOException
+  {
+    String result = null;
+    if (_definitionFile != null) 
+    {
+      File baseDir = getOwningTarget().getProject().getBaseDir();
+      result = Text.readTextFile(new File(baseDir, _definitionFile));
+    } else if (_dependencyDefinition.length() > 0)
+    {
+      result = _dependencyDefinition;
+    }
+    return result;
+  }
+}
