af19028132afe92a5973738e3c975d5aa43d589a
fjelmer <fjelmer@39be711e-0c1e-0410-9903-fe3f45c2df2f>
2008-05-23 16:24:36 +0000

    expand tests to test source feature

--- Classycle/srcTest/classycle/PackageProcessorTest.java
+++ Classycle/srcTest/classycle/PackageProcessorTest.java
@@ -16,11 +16,11 @@ public class PackageProcessorTest extends TestCase
   protected void setUp() throws Exception
   {
     
-    _a = new AtomicVertex(ClassAttributes.createClass("p.A", "", 1));
-    _b = new AtomicVertex(ClassAttributes.createClass("q.B", "", 2));
-    _c = new AtomicVertex(ClassAttributes.createClass("q.C", "", 3));
-    _d = new AtomicVertex(ClassAttributes.createClass("p.D", "", 4));
-    _o = new AtomicVertex(ClassAttributes.createClass("lang.Object", "", 42));
+    _a = new AtomicVertex(ClassAttributes.createClass("p.A", "s", 1));
+    _b = new AtomicVertex(ClassAttributes.createClass("q.B", "r", 2));
+    _c = new AtomicVertex(ClassAttributes.createClass("q.C", "s", 3));
+    _d = new AtomicVertex(ClassAttributes.createClass("p.D", "s", 4));
+    _o = new AtomicVertex(ClassAttributes.createClass("lang.Object", "s", 42));
     _o.setDefaultValueOfGraphVertexFlag(false);
     _a.addOutgoingArcTo(_o);
     _a.addOutgoingArcTo(_b);
@@ -44,6 +44,7 @@ public class PackageProcessorTest extends TestCase
     assertEquals(true, p.isGraphVertex());
     PackageAttributes attributes = (PackageAttributes) p.getAttributes();
     assertEquals("p", attributes.getName());
+    assertEquals("s", attributes.getSources());
     assertEquals(2, attributes.getSize());
     assertEquals(1, p.getNumberOfOutgoingArcs());
     Vertex packageLang = p.getHeadVertex(0);
@@ -54,6 +55,7 @@ public class PackageProcessorTest extends TestCase
     assertEquals(true, q.isGraphVertex());
     attributes = (PackageAttributes) q.getAttributes();
     assertEquals("q", attributes.getName());
+    assertEquals("r, s", attributes.getSources());
     assertEquals(2, attributes.getSize());
     assertEquals(2, q.getNumberOfOutgoingArcs());
     assertSame(packageLang, q.getHeadVertex(0));
